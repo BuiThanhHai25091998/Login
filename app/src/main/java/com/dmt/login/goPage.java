318d8c27f5a635af5e3b16b91966f97861e2e1eb
BuiThanhHai25091998 <48000109+BuiThanhHai25091998@users.noreply.github.com>
2020-07-10 14:36:58 +0700

    Thêm Phần Mới

--- app/src/main/java/com/dmt/login/goPage.java
+++ app/src/main/java/com/dmt/login/goPage.java
@@ -3,12 +3,26 @@ package com.dmt.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class goPage extends AppCompatActivity {
+    Button thoat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_page);
+        thoat = (Button) findViewById(R.id.thoat);
+        logOut();
+    }
+
+    private void logOut() {
+        thoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
