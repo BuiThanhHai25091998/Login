135fcc999346b4c12008190aae40ab6ff4b62eb2
BuiThanhHai25091998 <48000109+BuiThanhHai25091998@users.noreply.github.com>
2020-07-10 14:24:14 +0700

    Initial commit

---
+++ app/src/main/java/com/dmt/login/goPage.java
@@ -0,0 +1,14 @@
+package com.dmt.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class goPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_go_page);
+    }
+}
